f965308d83a788b082f5f70bc866ff5783834987
cti1650 <cti1650@gmail.com>
2021-04-26 00:31:42 +0900

    🐛 Tailwindが反映されないバグに対応

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,16 @@
 module.exports = {
   purge: [
     './src/components/**/*.js',
-    './pages/**/*.js'],
+    './pages/**/*.js',
+    './src/components/**/*.jsx',
+    './pages/**/*.jsx',
+  ],
   darkMode: false,
   theme: {
     minHeight: {
-      '60vh': '60vh'
-    }
+      '60vh': '60vh',
+    },
   },
   variants: {},
-  plugins: [
-    require( 'tailwindcss' ),
-    require( 'precss' ),
-    require( 'autoprefixer' )
-  ]
-}
+  plugins: [require('tailwindcss'), require('precss'), require('autoprefixer')],
+};
