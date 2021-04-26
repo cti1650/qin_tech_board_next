fb7d75afd53b369949db259048afd1914576942c
cti1650 <cti1650@gmail.com>
2021-04-27 01:21:17 +0900

    💄 フォームのサイズ変更

--- src/components/tools/GoogleForm.jsx
+++ src/components/tools/GoogleForm.jsx
@@ -6,7 +6,7 @@ function GoogleForm(props) {
       <iframe
         src="https://docs.google.com/forms/d/e/1FAIpQLScP5qjYvNSsm-AtHVm7uQOMXsrcvSoaRyJ9fuyJLF68fdqTNg/viewform?embedded=true"
         width="100%"
-        height="1600px"
+        height="2000px"
       >
         読み込んでいます…
       </iframe>
