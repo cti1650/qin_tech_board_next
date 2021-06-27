fdf3a240b71bae84355b87eddc58cf9d47f47b55
cti1650 <cti1650@gmail.com>
2021-06-27 19:52:39 +0900

    ✨ supabase導入

---
+++ pages/index2.js
@@ -0,0 +1,59 @@
+import Head from 'next/head';
+import { useState, useEffect } from 'react';
+import Layout from '../src/components/layout';
+import LinkButtons from '../src/components/button/LinkButtons';
+import LinkButtons2 from '../src/components/button/LinkButtons2';
+import { SupabaseDatas } from '../src/components/button/SupabaseDatas';
+import ScrollPageTop from '../src/components/tools/ScrollPageTop';
+import { supabase } from '../src/util/supabase';
+
+const updateDB = async () => {
+  return await supabase.from('types').select('*');
+};
+
+export default function Home() {
+  const [keyword, setKeyword] = useState('');
+  function doSearch(e) {
+    setKeyword(e.target.value);
+  }
+  const [linksData, setLinksData] = useState([]);
+  useEffect(async () => {
+    let DB = await updateDB();
+    setLinksData(DB.data);
+    supabase
+      .from('types')
+      .on('*', async (data) => {
+        let DB = await updateDB();
+        setLinksData(DB.data);
+      })
+      .subscribe();
+  }, []);
+  return (
+    <Layout>
+      <Head>
+        <title>QinTechBoard</title>
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+      <div className='w-full px-4 py-4 mt-4 sticky top-0 bg-black'>
+        <input
+          type='search'
+          className='w-full bg-black focus:bg-gray-900 outline-none rounded-full border border-gray-800 px-4 py-1 text-white'
+          placeholder='検索'
+          value={keyword}
+          onChange={(e) => doSearch(e)}
+        ></input>
+      </div>
+      {linksData &&
+        linksData.map((item) => (
+          <div>
+            <SupabaseDatas
+              table_id={item.name}
+              keyword={keyword}
+              small={item.word}
+            />
+          </div>
+        ))}
+      <ScrollPageTop></ScrollPageTop>
+    </Layout>
+  );
+}
---
+++ src/components/button/SupabaseDatas.jsx
@@ -0,0 +1,34 @@
+import { useState, useEffect } from 'react';
+import { supabase } from '../../util/supabase';
+import LinkButtons2 from '../button/LinkButtons2';
+import cc from "classcat";
+
+const updateDB = async (table_id) => {
+    return await supabase.from('links').select('*').eq('type',table_id);
+  };  
+
+export const SupabaseDatas = (props) => {
+  const { table_id,keyword,small } = props;
+  const [linksData, setLinksData] = useState([]);
+  useEffect(async () => {
+    let DB = await updateDB(table_id);
+    setLinksData(DB.data);
+    supabase
+      .from('links')
+      .on('*', async (data) => {
+        let DB = await updateDB(table_id);
+        setLinksData(DB.data);
+      })
+      .subscribe();
+  }, []);
+  return (
+    <>
+      <LinkButtons2
+        title={table_id}
+        items={linksData && (linksData || [])}
+        keyword={keyword}
+        size={small?'small':''}
+      ></LinkButtons2>
+    </>
+  )
+}
