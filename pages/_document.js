1cd85a060ed0ccd5bd002f14fe68a5af824dbcbf
cti1650 <cti1650@gmail.com>
2021-07-04 02:41:02 +0900

    🎉 Google Analytics導入

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,5 @@
 import Document, { Head, Main, NextScript } from 'next/document';
+import { existsGaId, GA_ID } from '../src/lib/gtag';
 
 export default class MyDocument extends Document {
   static async getInitialProps(ctx) {
@@ -8,9 +9,29 @@ export default class MyDocument extends Document {
 
   render() {
     return (
-      <html>
+      <html lang='ja'>
         <Head>
           <style>{`body { margin: 0 } /* custom!*/`}</style>
+          {/* Google Analytics */}
+          {existsGaId && (
+            <>
+              <script
+                async
+                src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
+              />
+              <script
+                dangerouslySetInnerHTML={{
+                  __html: `
+                  window.dataLayer = window.dataLayer || [];
+                  function gtag(){dataLayer.push(arguments);}
+                  gtag('js', new Date());
+                  gtag('config', '${GA_ID}', {
+                    page_path: window.location.pathname,
+                  });`,
+                }}
+              />
+            </>
+          )}
         </Head>
         <body className='custom_class w-full p-2 container mx-auto bg-black'>
           <Main />
