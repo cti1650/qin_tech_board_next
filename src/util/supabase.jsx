743593fd72fd03ee1381c4d11e61f1662e75ffaa
cti1650 <cti1650@gmail.com>
2021-07-17 23:37:09 +0900

    🔨アップロード機能をGASから直接へ切替

--- src/util/supabase.jsx
+++ src/util/supabase.jsx
@@ -1,4 +1,5 @@
 import { createClient } from '@supabase/supabase-js';
+import axios from 'axios';
 
 export const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL,
@@ -28,4 +29,86 @@ export const insertDB = async (json) => {
     .insert([data])
     .then((db) => ({ data: db.data, error: db.error }))
     .catch((e) => ({ data: [], error: e.statusCode }));
+};
+
+export const updateLinkUrl = async (url, option = {}) => {
+  let json = {
+    type: 'アップロード',
+    category: 'サイト',
+    url: url,
+  };
+  json = { ...json, ...option };
+  if (url) {
+    let data = await getUrlProperties(url);
+    if (data.title) json = { ...json, name: data.title };
+    if (data.description) json = { ...json, description: data.description };
+    if (data.keywords) json = { ...json, tag: data.keywords.join(',') };
+    await insertDB(json);
+    return json;
+  }
+  return json;
+};
+
+export const getUrlProperties = async (url) => {
+  let response = await axios.get(url);
+  // console.log(response.data);
+  let html = response.data;
+  let head = html.match(/<head(?:[\s\S]*?)>([\s\S]*?)<\/head>/);
+
+  let outputProps = {
+    url: url,
+    title: '',
+    description: '',
+    keywords: [],
+    'article:tag': [],
+    tags: [],
+  };
+
+  if (head) {
+    const headHtml = head[1];
+    let regs = [
+      { reg: /<(title)>([\s\S]*?)<\/title>/gi, name: 1, value: 2 },
+      {
+        reg: /<meta name=(?:"|')([\s\S]*?)(?:"|')(?:[\s]*?)content=(?:"|')([\s\S]*?)(?:"|')(?:[\s]*?)(?:\/)?>/gi,
+        name: 1,
+        value: 2,
+      },
+      {
+        reg: /<meta content=(?:"|')([\s\S]*?)(?:"|')(?:[\s]*?)(name|property)=(?:"|')([\s\S]*?)(?:"|')(?:[\s]*?)(?:\/)?>/gi,
+        name: 2,
+        value: 1,
+      },
+      {
+        reg: /<meta property=(?:"|')([\s\S]*?)(?:"|')(?:[\s]*?)content=(?:"|')([\s\S]*?)(?:"|')(?:[\s]*?)(?:\/)?>/gi,
+        name: 1,
+        value: 2,
+      },
+    ];
+
+    regs.map((item) => {
+      let match;
+      while ((match = item.reg.exec(headHtml)) != null) {
+        let name = match[item.name].toLowerCase();
+        let val = match[item.value] || '';
+        switch (name) {
+          case 'keywords':
+            val.split(',').map((item) => {
+              outputProps[name] = [...outputProps[name], ...[item]];
+            });
+            break;
+          case 'article:tag':
+            outputProps[name] = [...outputProps[name], ...[val]];
+            break;
+          default:
+            console.log(name + ' : ' + val);
+            outputProps[name] = val;
+        }
+      }
+    });
+
+    outputProps['tags'] = Array.from(
+      new Set([...outputProps['keywords'], ...outputProps['article:tag']])
+    );
+  }
+  return outputProps;
 };
