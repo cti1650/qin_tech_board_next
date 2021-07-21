b0a69ba038a9a532bdd461a12e9ec4439c2e0dae
cti1650 <cti1650@gmail.com>
2021-07-21 09:03:14 +0900

    CORS設定検証

---
+++ pages/api/util/link_props/test.js
@@ -0,0 +1,94 @@
+import axios from 'axios';
+import Cors from 'cors';
+
+const cors = Cors({
+  methods: ['GET', 'HEAD'],
+})
+
+function runMiddleware(req, res, fn) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result) => {
+      if (result instanceof Error) {
+        return reject(result)
+      }
+      return resolve(result)
+    })
+  })
+}
+
+async function handler(req, res) => {
+  await runMiddleware(req, res, cors);
+  return res.status(200).json(await getUrlProperties(req.query.url));
+};
+
+export default handler;
+
+const getUrlProperties = async (url) => {
+  let response = await axios.get(url);
+  // console.log(response.data);
+  let html = response.data;
+  let head = html.match(/<head(?:[\s\S]*?)>([\s\S]*?)<\/head>/);
+
+  let outputProps = {
+    url: url,
+    title: '',
+    description: '',
+    keywords: [],
+    'article:tag': [],
+    tags: [],
+  };
+
+  if (head) {
+    const headHtml = head[1];
+    let regs = [
+      { reg: /<(title)>([\s\S]*?)<\/title>/gi, name: 1, value: 2 },
+      {
+        reg: /<meta name=(?:"|')([\s\S]*?)(?:"|')(?:[\s]*?)content=(?:"|')([\s\S]*?)(?:"|')(?:[\s]*?)(?:\/)?>/gi,
+        name: 1,
+        value: 2,
+      },
+      {
+        reg: /<meta content=(?:"|')([\s\S]*?)(?:"|')(?:[\s]*?)(name|property)=(?:"|')([\s\S]*?)(?:"|')(?:[\s]*?)(?:\/)?>/gi,
+        name: 2,
+        value: 1,
+      },
+      {
+        reg: /<meta property=(?:"|')([\s\S]*?)(?:"|')(?:[\s]*?)content=(?:"|')([\s\S]*?)(?:"|')(?:[\s]*?)(?:\/)?>/gi,
+        name: 1,
+        value: 2,
+      },
+    ];
+
+    regs.map((item) => {
+      let match;
+      while ((match = item.reg.exec(headHtml)) != null) {
+        let name = match[item.name].toLowerCase();
+        let val = match[item.value] || '';
+        switch (name) {
+          case 'keywords':
+            val.split(',').map((item) => {
+              outputProps[name] = [...outputProps[name], ...[item]];
+            });
+            break;
+          case 'article:tag':
+            outputProps[name] = [...outputProps[name], ...[val]];
+            break;
+          case 'og:title':
+            outputProps.title = outputProps.title || val;
+            break;
+          case 'og:description':
+            outputProps.description = outputProps.description || val;
+            break;
+          default:
+            console.log(name + ' : ' + val);
+            outputProps[name] = val;
+        }
+      }
+    });
+
+    outputProps['tags'] = Array.from(
+      new Set([...outputProps['keywords'], ...outputProps['article:tag']])
+    );
+  }
+  return outputProps;
+};
