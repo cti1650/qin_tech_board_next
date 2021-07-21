cb3b62faa909f5a6e27d7d41210637b4e1164dc4
cti1650 <cti1650@gmail.com>
2021-07-21 10:54:03 +0900

    CORS検証結果反映

--- pages/api/util/link_add/index.js
+++ pages/api/util/link_add/index.js
@@ -1,8 +1,30 @@
 import { updateLinkUrl } from '@util/supabase';
+import Cors from 'cors';
 
-export default async (req, res) => {
+const cors = Cors({
+  methods: ['GET', 'POST', 'HEAD'],
+  origin: '*',
+  credentials: true,
+  optionsSuccessStatus: 200,
+})
+
+function runMiddleware(req, res, fn) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result) => {
+      if (result instanceof Error) {
+        return reject(result)
+      }
+      return resolve(result)
+    })
+  })
+}
+
+async function handler(req, res) {
+  await runMiddleware(req, res, cors);
   console.log(req);
   return res
     .status(200)
     .json(await updateLinkUrl(req.body.url || req.query.url));
 };
+
+export default handler;
